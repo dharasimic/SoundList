b1e7a3db63b41609911c0ee50db04ea26acc0321
kayo0404 <kayo.kcs@gmail.com>
2025-06-12 14:49:33 -0300

    Listagem de playlists corrigidas e adiciona a imagem na pagina corretamente

--- src/main/java/com/playlistar/playlistar/model/MusicaDAO.java
+++ src/main/java/com/playlistar/playlistar/model/MusicaDAO.java
@@ -33,8 +33,8 @@ public class MusicaDAO {//data access object
         jdbc.update(sql,parametros);
     }
 
-    public List<Map<String,Object>> puxarTodasMusicas(){
-        String sql = "SELECT * FROM musica;";
+    public List<Map<String, Object>> puxarTodasMusicas() {
+        String sql = "SELECT * FROM musica";
         return jdbc.queryForList(sql);
     }
 }
